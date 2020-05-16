6de0ee5ad35188f28d9c0cd51e5bbcaca7457785
AhmerovDmitry <62261655+AhmerovDmitry@users.noreply.github.com>
2020-05-16 18:16:18 +0300

    Final

--- HW2/HW2/ViewController.swift
+++ HW2/HW2/ViewController.swift
@@ -8,27 +8,33 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate{
     
     @IBOutlet weak var colorView: UIView!
     @IBOutlet var arrayTextField: [UITextField]!
     @IBOutlet var arrayLabel: [UILabel]!
     @IBOutlet var arraySlider: [UISlider]!
     
+    let colorNumber: Float = 255
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        // label settings
         arrayLabel[0].text = "0"
         arrayLabel[1].text = "0"
         arrayLabel[2].text = "0"
-        
-        colorView.backgroundColor = .blue
+        // view settings
         colorView.layer.cornerRadius = 20
-        
+        colorView.backgroundColor = .black
+        // slider settings
         valueRange(max: 1, and: 0)
-        
+        // text field settings
         for field in arrayTextField {
             field.keyboardType = .numberPad
         }
+        arrayTextField[0].addTarget(self, action: #selector(redTextFieldDidChange), for: .editingChanged)
+        arrayTextField[1].addTarget(self, action: #selector(redTextFieldDidChange), for: .editingChanged)
+        arrayTextField[2].addTarget(self, action: #selector(redTextFieldDidChange), for: .editingChanged)
         // remove keyboard use tap on view
         addTapGestureToHideKeyboard()
     }
@@ -39,31 +45,57 @@ class ViewController: UIViewController {
             value.minimumValue = min
         }
     }
-    
+    // UIAlertController
+    func showAlert(title: String, message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "OK", style: .default)
+        alert.addAction(okAction)
+        present(alert, animated: true)
+    }
+    //
+    @objc func redTextFieldDidChange() {
+        for index in 0...2 {
+            if let number = Float(arrayTextField[index].text!) {
+                arrayLabel[index].text = arrayTextField[index].text
+                arraySlider[index].value = number / colorNumber
+            } else {
+                arrayLabel[index].text = "0"
+                arraySlider[index].value = 0
+                arrayTextField[index].text = ""
+            }
+            
+            let maxNumberCount = 1...3
+            var numberCount = [String]()
+            for i in arrayTextField[index].text! {
+                numberCount.append(String(i))
+            }
+            
+            if let textNumber = Int(arrayTextField[index].text!) {
+                if textNumber <= 255 && maxNumberCount.contains(numberCount.count) {
+                    // UIAlertAction
+                } else {
+                    showAlert(title: "Unknown number", message: "Need to write a new number")
+                    arrayLabel[index].text = "0"
+                    arraySlider[index].value = 0
+                    arrayTextField[index].text = ""
+                }
+            }
+        }
+        colorView.backgroundColor = UIColor(red: CGFloat(Float(arrayLabel[0].text!)! / colorNumber), green: CGFloat(Float(arrayLabel[1].text!)! / colorNumber), blue: CGFloat(Float(arrayLabel[2].text!)! / colorNumber), alpha: 1)
+        arraySlider[0].minimumTrackTintColor = UIColor(red: CGFloat(Float(arrayLabel[0].text!)! / colorNumber), green: 0, blue: 0, alpha: 1)
+        arraySlider[1].minimumTrackTintColor = UIColor(red: 0, green: CGFloat(Float(arrayLabel[1].text!)! / colorNumber), blue: 0, alpha: 1)
+        arraySlider[2].minimumTrackTintColor = UIColor(red: 0, green: 0, blue: CGFloat(Float(arrayLabel[2].text!)! / colorNumber), alpha: 1)
+    }
+    // slider
     @IBAction func sliderAction() {
         for index in 0...2 {
-            arrayLabel[index].text = String(Int(arraySlider[index].value * 255))
-            arrayTextField[index].text = String(Int(arraySlider[index].value * 255))
+            arrayTextField[index].text = String(Int(arraySlider[index].value * colorNumber))
+            arrayLabel[index].text = arrayTextField[index].text
         }
-        arraySlider[0].minimumTrackTintColor = UIColor(red: CGFloat((arraySlider[0].value)), green: 0, blue: 0, alpha: 1)
-        arraySlider[1].minimumTrackTintColor = UIColor(red: 0, green: CGFloat((arraySlider[1].value)), blue: 0, alpha: 1)
-        arraySlider[2].minimumTrackTintColor = UIColor(red: 0, green: 0, blue: CGFloat((arraySlider[2].value)), alpha: 1)
-        
-        colorView.backgroundColor = UIColor(red: CGFloat(arraySlider[0].value),
-                                            green: CGFloat(arraySlider[1].value),
-                                            blue: CGFloat(arraySlider[2].value),
-                                            alpha: 1)
-        inputData()
-    }
-    
-    func inputData() {
-        arraySlider[0].value = Float(arrayTextField[0].text!)! / 255
-        arraySlider[1].value = Float(arrayTextField[1].text!)! / 255
-        arraySlider[2].value = Float(arrayTextField[2].text!)! / 255
-        
-        print(arraySlider[0].value)
-        print(arraySlider[1].value)
-        print(arraySlider[2].value)
+        colorView.backgroundColor = UIColor(red: CGFloat(Float(arrayLabel[0].text!)! / colorNumber), green: CGFloat(Float(arrayLabel[1].text!)! / colorNumber), blue: CGFloat(Float(arrayLabel[2].text!)! / colorNumber), alpha: 1)
+        arraySlider[0].minimumTrackTintColor = UIColor(red: CGFloat(Float(arrayLabel[0].text!)! / colorNumber), green: 0, blue: 0, alpha: 1)
+        arraySlider[1].minimumTrackTintColor = UIColor(red: 0, green: CGFloat(Float(arrayLabel[1].text!)! / colorNumber), blue: 0, alpha: 1)
+        arraySlider[2].minimumTrackTintColor = UIColor(red: 0, green: 0, blue: CGFloat(Float(arrayLabel[2].text!)! / colorNumber), alpha: 1)
     }
 }
 // remove keyboard use tap on view
