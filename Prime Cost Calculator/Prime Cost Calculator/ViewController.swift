92abdbb8f931086f046d96b9ec6315d4b2473e6c
AhmerovDmitry <62261655+AhmerovDmitry@users.noreply.github.com>
2020-05-18 21:00:55 +0300

    Update 1.0.2
    1. New button for result
    2. New calculated for result

--- Prime Cost Calculator/Prime Cost Calculator/ViewController.swift
+++ Prime Cost Calculator/Prime Cost Calculator/ViewController.swift
@@ -11,27 +11,30 @@ import UIKit
 class ViewController: UIViewController {
     @IBOutlet var arrayProductTextField: [UITextField]!
     @IBOutlet var calcTextField: [UITextField]!
+    @IBOutlet weak var resultButtonOutlet: UIButton!
     @IBOutlet weak var result: UILabel!
-    
-    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         addTapGestureToHideKeyboards()
+        
+        resultButtonOutlet.layer.cornerRadius = 10
     }
 
     @IBAction func calculation() {
+        let numberCount = 4
         for index in 0...2 {
-            if calcTextField[index].text == nil {
-                print("nil")
-            } else {
+            if calcTextField[index].text!.count > numberCount {
                 calcTextField[index].text = "0"
             }
         }
     }
     
-    @objc func redTextFieldDidChange() {}
+    @IBAction func resultButton() {
+        result.text = String(Int(calcTextField[1].text!)! * Int(calcTextField[2].text!)! / Int(calcTextField[0].text!)!)
+    }
     
+    //@objc func redTextFieldDidChange() {}
     func addTapGestureToHideKeyboards() {
         let tapGesture = UITapGestureRecognizer(target: view, action: #selector(view.endEditing))
         view.addGestureRecognizer(tapGesture)
