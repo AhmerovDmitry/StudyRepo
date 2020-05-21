af161b936156ae4b0d0e0e6895c6d3705637d883
AhmerovDmitry <62261655+AhmerovDmitry@users.noreply.github.com>
2020-05-21 10:48:30 +0300

    Not significant updates
    1. New app for home work

---
+++ About Me/About Me/Controller/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  About Me
+//
+//  Created by Дмитрий Ахмеров on 26 Iyar 5780.
+//  Copyright © 5780 Дмитрий Ахмеров. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
