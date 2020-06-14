1c28f651174a440752a4883854c044bd2b0f8d21
AhmerovDmitry <62261655+AhmerovDmitry@users.noreply.github.com>
2020-06-14 22:47:50 +0800

    Test install framework

---
+++ HW9/HW9/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HW9
+//
+//  Created by Дмитрий Ахмеров on 13.06.2020.
+//  Copyright © 2020 Дмитрий Ахмеров. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
