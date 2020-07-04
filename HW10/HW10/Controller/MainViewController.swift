8ae83b302b68e994c27c82198c178d434ccaabf8
AhmerovDmitry <62261655+AhmerovDmitry@users.noreply.github.com>
2020-07-04 16:38:53 +0300

    First ver.

---
+++ HW10/HW10/Controller/MainViewController.swift
@@ -0,0 +1,53 @@
+//
+//  MainViewController.swift
+//  HW10
+//
+//  Created by Дмитрий Ахмеров on 04.07.2020.
+//  Copyright © 2020 Дмитрий Ахмеров. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UITableViewController {
+    
+    let networkService = NetworkService()
+    var searachResponse: JsonCharacters? = nil
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let jsonUrl = "https://rickandmortyapi.com/api/character"
+
+        networkService.fetchData(urlString: jsonUrl) { [weak self] (result) in
+            switch result {
+                
+            case .success(let searchResponse):
+                self?.searachResponse = searchResponse
+                self?.tableView.reloadData()
+            case .failure(let error):
+                print("error:", error)
+            }
+        }
+    }
+
+    // MARK: - Table view data source
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return searachResponse?.results?.count ?? 0
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        let name = searachResponse?.results![indexPath.row]
+        
+        cell.textLabel?.text = name?.name
+        cell.detailTextLabel?.text = name?.species
+        
+        
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+}
---
+++ HW10/HW10/Models/api.swift
@@ -0,0 +1,17 @@
+//
+//  api.swift
+//  HW10
+//
+//  Created by Дмитрий Ахмеров on 04.07.2020.
+//  Copyright © 2020 Дмитрий Ахмеров. All rights reserved.
+//
+
+struct JsonCharacters: Decodable {
+    let results: [Character]?
+}
+
+struct Character: Decodable {
+    let name: String?
+    let image: String?
+    let species: String?
+}
