6ed0dfd0ccc4c4e2b3e0eba6e73a9f7d3583d2a6
AhmerovDmitry <62261655+AhmerovDmitry@users.noreply.github.com>
2020-08-16 16:05:58 +0300

    Update
    1. Min and max count logic
    2. Alarm else max number > 99

--- MagicBall/MagicBall/RandomNumber.swift
+++ MagicBall/MagicBall/RandomNumber.swift
@@ -10,29 +10,58 @@ import UIKit
 
 extension ViewController {
     
-    // MARK: - Method for call random number
-    
-    func randomNumber() -> Int {
+    // MARK: - Min and max number
+    func restrictions() -> Int {
         var result = 0
+        guard let numberOne = Int(firstTextField.text!) else { return result }
+        guard let numberTwo = Int(secondTextField.text!) else { return result }
         
-        if let firstNumber = Int(firstTextField.text!), let secondNumber = Int(secondTextField.text!) {
-            if firstNumber < secondNumber {
-                result = Int.random(in: firstNumber...secondNumber)
-            } else if firstNumber > secondNumber {
-                result = Int.random(in: secondNumber...firstNumber)
-            }
-        } else {
+        if (numberOne < minNumber || numberOne > maxNumber) || (numberTwo < minNumber || numberTwo > maxNumber) {
             alarmController()
+            
+            firstTextField.text = ""
+            secondTextField.text = ""
+        } else {
+            if let firstNumber = Int(firstTextField.text!), let secondNumber = Int(secondTextField.text!) {
+                if firstNumber < secondNumber {
+                    result = Int.random(in: firstNumber...secondNumber)
+                } else if firstNumber > secondNumber {
+                    result = Int.random(in: secondNumber...firstNumber)
+                }
+            } else {
+                alarmController()
+            }
         }
         return result
     }
     
-    // MARK: - Alert Controller
+    // MARK: - Settings for TextFields counts
+    @objc func firstNumberCount() {
+        var firstTextFieldCount = [String]()
+        for value in firstTextField.text! {
+            firstTextFieldCount.append(String(value))
+        }
+        if firstTextFieldCount.count > 2 {
+             alarmController()
+             firstTextField.text = ""
+        }
+    }
     
+    @objc func secondNumberCount() {
+        var secondTextFieldCount = [String]()
+        for value in secondTextField.text! {
+            secondTextFieldCount.append(String(value))
+        }
+        if secondTextFieldCount.count > 2 {
+             alarmController()
+             secondTextField.text = ""
+        }
+    }
+    
+    // MARK: - Alert Controller
     func alarmController() {
         let alert = UIAlertController(title: "Ошибка!", message: "Проверьте правильность чисел.", preferredStyle: .alert)
         alert.addAction(UIAlertAction(title: "Отмена", style: .default))
         self.present(alert, animated: true)
     }
-    
 }
--- MagicBall/MagicBall/ViewController.swift
+++ MagicBall/MagicBall/ViewController.swift
@@ -11,15 +11,13 @@ import UIKit
 class ViewController: UIViewController, UIGestureRecognizerDelegate {
     
     // MARK: - @IBOutlets
-    
     @IBOutlet weak var numberInBall: UILabel!
     @IBOutlet weak var firstTextField: UITextField!
     @IBOutlet weak var secondTextField: UITextField!
     
     // MARK: - @IBActions
-    
     @IBAction func resultButton(_ sender: UIButton) {
-        numberInBall.text = String(randomNumber())
+        numberInBall.text = String(restrictions())
     }
     
     @IBAction func tap(_ sender: UITapGestureRecognizer) {
@@ -27,4 +25,16 @@ class ViewController: UIViewController, UIGestureRecognizerDelegate {
         secondTextField.resignFirstResponder()
     }
     
+    // MARK: - Properties
+    let minNumber = 0
+    let maxNumber = 99
+}
+
+extension ViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        firstTextField.addTarget(self, action: #selector(firstNumberCount), for: .editingChanged)
+        secondTextField.addTarget(self, action: #selector(secondNumberCount), for: .editingChanged)
+    }
 }
