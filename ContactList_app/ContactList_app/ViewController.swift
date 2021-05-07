e9c3d0440dd0dbcf767ce44fec4962c4a604231d
AhmerovDmitry <62261655+AhmerovDmitry@users.noreply.github.com>
2021-05-07 10:40:05 +0300

    ContactList_app

---
+++ ContactList_app/ContactList_app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ContactList_app
+//
+//  Created by Дмитрий Ахмеров on 07.05.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
